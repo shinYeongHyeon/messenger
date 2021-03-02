0083700a31b34ec49c907a256b3dbc6274abeb80
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-03-02 13:28:22 +0900

    Message Component (w/ Styled)

--- src/component/Chat.svelte
+++ src/component/Chat.svelte
@@ -1,3 +1,7 @@
+<script>
+  import Message from './Message.svelte';
+</script>
+
 <div class="min-w-max h-screen flex flex-col bg-transparent mb-4">
-    Chat Section
+    <Message />
 </div>
---
+++ src/component/Message.svelte
@@ -0,0 +1,7 @@
+<div class="flex">
+    <div class="flex-initial p-2 rounded-md bg-gray-50 bg-opacity-50">
+        메시지 내용 <br>
+        메시지 내용 <br>
+    </div>
+    <div class="flex items-end text-xs ml-1">오후 04:23</div>
+</div>

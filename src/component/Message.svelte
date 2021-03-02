88f5f1338595863273a028caea18b33e53d2af62
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-03-03 00:44:37 +0900

    Connect Component (w/ Store)

--- src/component/Message.svelte
+++ src/component/Message.svelte
@@ -1,7 +1,14 @@
-<div class="flex">
-    <div class="flex-initial p-2 rounded-md bg-gray-50 bg-opacity-50">
-        메시지 내용 <br>
-        메시지 내용 <br>
+<script>
+    import { messages } from '../stores/stores';
+</script>
+
+{ #each $messages as message }
+    <div class="flex mb-2">
+        <div class="flex-initial p-2 rounded-md bg-gray-50 bg-opacity-50">
+            { message.messageValue }
+        </div>
+        <div class="flex items-end text-xs ml-1">{ message.datetime }</div>
     </div>
-    <div class="flex items-end text-xs ml-1">오후 04:23</div>
-</div>
+{ /each }
+
+
--- src/component/MessageInput.svelte
+++ src/component/MessageInput.svelte
@@ -1,6 +1,27 @@
+<script>
+  import { messages } from '../stores/stores';
+
+  let messageValue = '';
+
+  function sendMessage() {
+    $messages = [...$messages, {
+      messageValue,
+      datetime: new Date(),
+    }]
+
+    messageValue = '';
+  }
+</script>
+
 <div class="h-20 rounded-md flex flex-row bg-white p-1">
     <label class="flex-grow mr-2">
-        <textarea class="w-full h-full resize-none focus:outline-none"></textarea>
+        <textarea
+                class="w-full h-full resize-none focus:outline-none"
+                bind:value={ messageValue }
+        ></textarea>
     </label>
-    <button class="w-16 bg-gray-100 rounded-lg text-sm font-semibold">전&nbsp;송</button>
+    <button
+            class="w-16 bg-gray-100 rounded-lg text-sm font-semibold"
+            on:click={ sendMessage }
+    >전&nbsp;송</button>
 </div>
